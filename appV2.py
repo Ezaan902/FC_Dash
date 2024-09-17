26610aa89f40cad0c951b766e5ea99b0d82d1746
Axel BENOIT <39768836+Ezaan902@users.noreply.github.com>
2024-09-17 15:22:20 +0200

    Update appV2 removing port.py

--- appV2.py
+++ appV2.py
@@ -536,6 +536,6 @@ def update_dash(origine, destination, formation, flux_value, layer_toggle):
     )
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8052)
+    app.run_server(debug=True)
 
 
